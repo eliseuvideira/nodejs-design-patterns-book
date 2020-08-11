1607edf3e5a0c193e296976230a8f232e8ad59ce
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-10 21:59:06 -0300

    unleashing the zalgo

---
+++ chapter03/inconsistent-function/index.js
@@ -0,0 +1,58 @@
+const { readFile } = require("fs");
+
+const cache = {};
+
+const inconsistentRead = (filename, cb) => {
+  if (cache[filename]) {
+    cb(cache[filename]);
+    return;
+  }
+  readFile(filename, "utf8", (err, data) => {
+    cache[filename] = data;
+    cb(data);
+  });
+};
+
+const consistentRead = (filename, cb) => {
+  if (cache[filename]) {
+    setTimeout(() => cb(cache[filename]), 0);
+    return;
+  }
+  readFile(filename, "utf8", (err, data) => {
+    cache[filename] = data;
+    cb(data);
+  });
+};
+
+const createFileReader = (filename) => {
+  const listeners = [];
+
+  // inconsistentRead(filename, (value) => {
+  //   listeners.forEach((listener) => {
+  //     console.log("sync");
+  //     listener(value);
+  //   });
+  // });
+
+  consistentRead(filename, (value) => {
+    listeners.forEach((listener) => {
+      console.log("async");
+      listener(value);
+    });
+  });
+
+  return {
+    onDataReady: (listener) => listeners.push(listener),
+  };
+};
+
+const reader1 = createFileReader(require.resolve("../../package.json"));
+
+reader1.onDataReady((data) => {
+  console.log(`First call data: ${data}`);
+
+  const reader2 = createFileReader(require.resolve("../../package.json"));
+  reader2.onDataReady((data) => {
+    console.log(`Second call data: ${data}`);
+  });
+});
